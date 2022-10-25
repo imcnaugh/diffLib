689312b937758a3b63330f98aae5b4c32ab8b1f1
Ian McNaughton <IanMcNaugh@gmail.com>
2022-10-25 14:52:26 -0500

    ugh so much refactoring, im still not happy as it only finds the largest diff, I need to work on it yet, but this feels cleaner and closer to what im looking for

--- src/main/diff.js
+++ src/main/diff.js
@@ -1,38 +1,55 @@
 const START_DIFF_TAG = '['
 const END_DIFF_TAG = ']'
 const EXPECTED_ACTUAL_SEPARATOR = '|'
-const ENCODED_START_DIFF_TAG = '[['
-const ENCODED_END_DIFF_TAG = ']]'
-const ENCODED_EXPECTED_ACTUAL_SEPARATOR = '||'
+const ENCODED_START_DIFF_TAG = START_DIFF_TAG + START_DIFF_TAG
+const ENCODED_END_DIFF_TAG = END_DIFF_TAG + END_DIFF_TAG
+const ENCODED_EXPECTED_ACTUAL_SEPARATOR = EXPECTED_ACTUAL_SEPARATOR + EXPECTED_ACTUAL_SEPARATOR
 
 function Diff(expected, actual) {
     this.expected = encodeString(expected)
     this.actual = encodeString(actual)
     this.isEqual = this.expected === this.actual
-    this.diffString = this.isEqual ? this.actual : getDiffString(this.expected, this.actual)
+}
+
+Diff.prototype.diffByLetter = function() {
+    const sepereator = ''
+    if(this.isEqual) return this.actual
+    return createDiffString(this.expected.split(sepereator), this.actual.split(sepereator), sepereator)
+}
+
+Diff.prototype.diffByWord = function() {
+    const sepereator = ' '
+    if(this.isEqual) return this.actual
+    return createDiffString(this.expected.split(sepereator), this.actual.split(sepereator), sepereator)
+}
+
+Diff.prototype.diffByLine = function() {
+    const sepereator = '\n'
+    if(this.isEqual) return this.actual
+    return createDiffString(this.expected.split(sepereator), this.actual.split(sepereator), sepereator)
+}
+
+function createDiffString(expected, actual, sepereator) {
+    let prefixCommonCount = findPrefixCommonCharCount(expected, actual)
+    let postfixCommonCount = findPostfixCommonCharsCount(expected, actual)
+
+    const commonPrefix = expected.slice(0, prefixCommonCount).join(sepereator)
+    const expectedDiff = expected.slice(prefixCommonCount, expected.length - postfixCommonCount).join(sepereator)
+    const acutalDiff = actual.slice(prefixCommonCount, actual.length - postfixCommonCount).join(sepereator)
+    const commonPostfix = expected.slice(expected.length - postfixCommonCount).join(sepereator)
+    return commonPrefix + (commonPrefix.length > 0 ? sepereator : '') +
+        START_DIFF_TAG +
+        expectedDiff +
+        EXPECTED_ACTUAL_SEPARATOR +
+        acutalDiff +
+        END_DIFF_TAG + (commonPostfix.length > 0 ? sepereator: '') +
+        commonPostfix
 }
 
 function encodeString(str) {
     return str.toString().replace(START_DIFF_TAG , ENCODED_START_DIFF_TAG)
         .replace(END_DIFF_TAG , ENCODED_END_DIFF_TAG)
         .replace(EXPECTED_ACTUAL_SEPARATOR , ENCODED_EXPECTED_ACTUAL_SEPARATOR)
-}
-
-function getDiffString(expected, actual) {
-    let prefixCommonCharCount = findPrefixCommonCharCount(expected, actual)
-    let postfixCommonCharCount = findPostfixCommonCharsCount(expected, actual)
-
-    const commonPrefix = expected.substring(0, prefixCommonCharCount)
-    const expectedDiff = expected.substring(prefixCommonCharCount, expected.length - postfixCommonCharCount)
-    const acutalDiff = actual.substring(prefixCommonCharCount, actual.length - postfixCommonCharCount)
-    const commonPostfix = expected.substring(expected.length - postfixCommonCharCount)
-    return commonPrefix + 
-        START_DIFF_TAG + 
-        expectedDiff + 
-        EXPECTED_ACTUAL_SEPARATOR + 
-        acutalDiff + 
-        END_DIFF_TAG + 
-        commonPostfix
 }
 
 function findPrefixCommonCharCount(expected, actual) {
--- src/test/diff.test.js
+++ src/test/diff.test.js
@@ -1,87 +1,106 @@
 const Diff = require('../main/diff.js')
 
-test('should return true when s1 is equal to s2', () => {
-    const diff = new Diff('1', '1')
-    expect(diff.isEqual).toBe(true)
+describe('Verify diff paramaters', () => {
+    test('should return true when s1 is equal to s2', () => {
+        const diff = new Diff('1', '1')
+        expect(diff.isEqual).toBe(true)
+    })
+    
+    test('should return false when s1 is not equal to s2', () => {
+        const diff = new Diff('1', '2')
+        expect(diff.isEqual).toBe(false)
+    })
+    
+    test('diff should remember expected and acutal', () => {
+        const diff = new Diff('1', '2')
+        expect(diff.expected).toBe('1')
+        expect(diff.actual).toBe('2')
+    })
 })
 
-test('should return false when s1 is not equal to s2', () => {
-    const diff = new Diff('1', '2')
-    expect(diff.isEqual).toBe(false)
+describe('Verify diff by letter', () => {
+    test('should show the diff of a single world', () => {
+        const diff = new Diff('abc', 'adc')
+        expect(diff.diffByLetter()).toBe('a[b|d]c')
+    })
+
+    test('should show expected empty when actualy has more chars', () => {
+        const diff = new Diff('ac', 'adc')
+        expect(diff.diffByLetter()).toBe('a[|d]c')
+    })
+    
+    test('should show actual empty when expected has more chars', () => {
+        const diff = new Diff('adc', 'ac')
+        expect(diff.diffByLetter()).toBe('a[d|]c')
+    })
+    
+    test('empty strings should be handled appropriately', () => {
+        const diff = new Diff('', '')
+        expect(diff.diffByLetter()).toBe('')
+    })
+    
+    test('empty expected should be handled appropriately', () => {
+        const diff = new Diff('', 'abc')
+        expect(diff.diffByLetter()).toBe('[|abc]')
+    })
+    
+    test('empty actual should be handled appropriately', () => {
+        const diff = new Diff('abc', '')
+        expect(diff.diffByLetter()).toBe('[abc|]')
+    })
+    
+    test('special chars should be encoded correctly', () => {
+        const diff = new Diff('[]|', '[]|')
+        expect(diff.diffByLetter()).toBe('[[]]||')
+    })
+    
+    test('diff strings with special chars should be encoded and diffed correctly', () => {
+        const diff = new Diff('[]|', '[abc]|')
+        expect(diff.diffByLetter()).toBe('[[[|abc]]]||')
+    })
+
+    test('a difference of case should show up as a diff', () => {
+        const diff = new Diff('A', 'a')
+        expect(diff.diffByLetter()).toBe('[A|a]')
+    })
 })
 
-test('diff should remember expected and acutal', () => {
-    const diff = new Diff('1', '2')
-    expect(diff.expected).toBe('1')
-    expect(diff.actual).toBe('2')
+describe('Verify diff by word', () =>{
+    test('verify that diffing by words works correcctly', () =>{
+        const s1 = 'words but some are misspelled';
+        const s2 = 'words but some are misselled';
+        const diff = new Diff(s1, s2)
+        expect(diff.diffByWord()).toBe('words but some are [misspelled|misselled]')
+    })
 })
 
-test('should show the diff of a single world', () => {
-    const diff = new Diff('abc', 'adc')
-    expect(diff.diffString).toBe('a[b|d]c')
+describe('Verify diff by line', () => {
+    test('Simple line diff test', () => {
+        const s1 = 'aaa\nbbb\nccc'
+        const s2 = 'aaa\nccc'
+        const diff = new Diff(s1, s2)
+        expect(diff.diffByLine()).toBe('aaa\n[bbb|]\nccc')
+    })
 })
 
-test('should show expected empty when actualy has more chars', () => {
-    const diff = new Diff('ac', 'adc')
-    expect(diff.diffString).toBe('a[|d]c')
-})
-
-test('should show actual empty when expected has more chars', () => {
-    const diff = new Diff('adc', 'ac')
-    expect(diff.diffString).toBe('a[d|]c')
-})
-
-test('empty strings should be handled appropriately', () => {
-    const diff = new Diff('', '')
-    expect(diff.diffString).toBe('')
-})
-
-test('empty expected should be handled appropriately', () => {
-    const diff = new Diff('', 'abc')
-    expect(diff.diffString).toBe('[|abc]')
-})
-
-test('empty actual should be handled appropriately', () => {
-    const diff = new Diff('abc', '')
-    expect(diff.diffString).toBe('[abc|]')
-})
-
-test('special chars should be encoded correctly', () => {
-    const diff = new Diff('[]|', '[]|')
-    expect(diff.diffString).toBe('[[]]||')
-})
-
-test('diff strings with special chars should be encoded and diffed correctly', () => {
-    const diff = new Diff('[]|', '[abc]|')
-    expect(diff.diffString).toBe('[[[|abc]]]||')
-})
-
-test('should show diff of longer strings', () => {
-    const diff = new Diff('this is a longer string it has a few words', 'this s a longer string it has a fe words')
-    expect(diff.diffString).toBe('this [is a longer string it has a few|s a longer string it has a fe] words')
-})
-
-test('a difference of case should show up as a diff', () => {
-    const diff = new Diff('A', 'a')
-    expect(diff.diffString).toBe('[A|a]')
-})
-
-test('ints should be diffed propertly', () => {
-    const diff = new Diff(1, 2)
-    expect(diff.diffString).toBe('[1|2]')
-})
-
-test('two of the same ints should diff properly', () => {
-    const diff = new Diff(0, 0)
-    expect(diff.diffString).toBe('0')
-})
-
-test('what happens when an int and a string are diffed', () => {
-    const diff = new Diff(1, '1')
-    expect(diff.diffString).toBe('1')
-})
-
-test('what happens when large ints and a string are diffed', () => {
-    const diff = new Diff(1001, '1001')
-    expect(diff.diffString).toBe('1001')
+describe('Verify some edge cases are handled', () => {
+    test('ints should be diffed propertly', () => {
+        const diff = new Diff(1, 2)
+        expect(diff.diffByLetter()).toBe('[1|2]')
+    })
+    
+    test('two of the same ints should diff properly', () => {
+        const diff = new Diff(0, 0)
+        expect(diff.diffByLetter()).toBe('0')
+    })
+    
+    test('what happens when an int and a string are diffed', () => {
+        const diff = new Diff(1, '1')
+        expect(diff.diffByLetter()).toBe('1')
+    })
+    
+    test('what happens when large ints and a string are diffed', () => {
+        const diff = new Diff(1001, '1001')
+        expect(diff.diffByLetter()).toBe('1001')
+    })
 })
