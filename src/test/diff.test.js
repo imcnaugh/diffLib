59f7e24d092d61cfdd62f5ab9768eede8e20340f
Ian McNaughton <IanMcNaugh@gmail.com>
2022-10-26 15:22:35 -0500

    added new test

--- src/test/diff.test.js
+++ src/test/diff.test.js
@@ -95,6 +95,13 @@ describe('Verify diff by line', () => {
         const diff = new Diff(s1, s2)
         expect(diff.diffByLine()).toBe('aaa\n[bbb|]\nccc')
     })
+
+    test('a string of words 1 char off should flag the whole line', () => {
+        const s1 = 'words but some are misspelled';
+        const s2 = 'word but some are misspelled';
+        const diff = new Diff(s1, s2)
+        expect(diff.diffByLine()).toBe('[words but some are misspelled|word but some are misspelled]')
+    })
 })
 
 describe('Verify some edge cases are handled', () => {
