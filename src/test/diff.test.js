3b56aad12b8b5e45dc062053e152d3a4b4b3a280
Ian McNaughton <IanMcNaugh@gmail.com>
2022-10-27 20:52:39 -0500

    added new test for saving the delimiter

--- src/test/diff.test.js
+++ src/test/diff.test.js
@@ -2,71 +2,24 @@ const Diff = require('../main/diff.js')
 
 describe('Verify diff paramaters', () => {
     test('should return true when s1 is equal to s2', () => {
-        const diff = new Diff('1', '1')
+        const diff = new Diff('1', '1', '')
         expect(diff.isEqual).toBe(true)
     })
     
     test('should return false when s1 is not equal to s2', () => {
-        const diff = new Diff('1', '2')
+        const diff = new Diff('1', '2', '')
         expect(diff.isEqual).toBe(false)
     })
     
     test('diff should remember expected and acutal', () => {
-        const diff = new Diff('1', '2')
+        const diff = new Diff('1', '2', '')
         expect(diff.expected).toBe('1')
         expect(diff.actual).toBe('2')
     })
-})
 
-describe('Verify diff by letter', () => {
-    test('two equal strings should not have a diff', () => {
-        const diff = new Diff('1', '1')
-        expect(diff.diffByLetter()).toBe('1')
-    })
-
-    test('should show the diff of a single world', () => {
-        const diff = new Diff('abc', 'adc')
-        expect(diff.diffByLetter()).toBe('a[b|d]c')
-    })
-
-    test('should show expected empty when actualy has more chars', () => {
-        const diff = new Diff('ac', 'adc')
-        expect(diff.diffByLetter()).toBe('a[|d]c')
-    })
-    
-    test('should show actual empty when expected has more chars', () => {
-        const diff = new Diff('adc', 'ac')
-        expect(diff.diffByLetter()).toBe('a[d|]c')
-    })
-    
-    test('empty strings should be handled appropriately', () => {
-        const diff = new Diff('', '')
-        expect(diff.diffByLetter()).toBe('')
-    })
-    
-    test('empty expected should be handled appropriately', () => {
-        const diff = new Diff('', 'abc')
-        expect(diff.diffByLetter()).toBe('[|abc]')
-    })
-    
-    test('empty actual should be handled appropriately', () => {
-        const diff = new Diff('abc', '')
-        expect(diff.diffByLetter()).toBe('[abc|]')
-    })
-    
-    test('special chars should be encoded correctly', () => {
-        const diff = new Diff('[]|', '[]|')
-        expect(diff.diffByLetter()).toBe('[[]]||')
-    })
-    
-    test('diff strings with special chars should be encoded and diffed correctly', () => {
-        const diff = new Diff('[]|', '[abc]|')
-        expect(diff.diffByLetter()).toBe('[[[|abc]]]||')
-    })
-
-    test('a difference of case should show up as a diff', () => {
-        const diff = new Diff('A', 'a')
-        expect(diff.diffByLetter()).toBe('[A|a]')
+    test('diff should remember the delimiter', () => {
+        const diff = new Diff('1', '2', 'x')
+        expect(diff.sepereator).toBe('x')
     })
 })
 
