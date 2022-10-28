898793ca5e5968b1b6dbddf79f5defae2cc35f8b
Ian McNaughton <IanMcNaugh@gmail.com>
2022-10-28 15:00:12 -0500

    general refactoring

--- src/main/diff.js
+++ src/main/diff.js
@@ -18,18 +18,25 @@ Diff.prototype.getDiffString = function() {
     this.diffParts = []
     this.remainingExpected = this.encodedExpected.split(this.delimiter)
     this.remainingActual = this.encodedActual.split(this.delimiter)
-    this.createDiffString()
+    this.generateDiffParts()
     return this.diffParts.filter(Boolean).join(this.delimiter)
 }
 
-Diff.prototype.createDiffString = function() {
+Diff.prototype.generateDiffParts = function() {
     if(this.remainingExpected.length === 0 && this.remainingActual.length === 0) return
     this.addNextPrefix()
     this.addNextDiff()
-    this.createDiffString()
+    this.generateDiffParts()
 }
 
 Diff.prototype.addNextPrefix = function() {
+    const indexOfNextDiff = this.findIndexOfNextDiff()
+    const commonPrefix = this.remainingActual.slice(0, indexOfNextDiff).join(this.delimiter)
+    this.sliceRemaining(indexOfNextDiff, indexOfNextDiff)
+    this.diffParts.push(commonPrefix)
+}
+
+Diff.prototype.findIndexOfNextDiff = function() {
     let minLenght = Math.min(this.remainingExpected.length, this.remainingActual.length)
     let i = 0
     for (; i < minLenght; i++) {
@@ -37,54 +44,50 @@ Diff.prototype.addNextPrefix = function() {
             break
         }
     }
-
-    this.diffParts.push(this.remainingActual.slice(0,i).join(this.delimiter))
-    this.sliceRemaining(i, i)
+    return i
 }
 
 Diff.prototype.addNextDiff = function() {
-    let {expectedRemainingPostDiffIndex, actualRemainingPostDiffIndex} = this.findIndexesForNextDiff()
+    let {expectedNextCommonIndex, actualNextCommonIndex} = this.findIndexOfNextCommon()
 
-    const diffOfExpected = this.remainingExpected.slice(0, expectedRemainingPostDiffIndex)
-    const diffOfActual = this.remainingActual.slice(0, actualRemainingPostDiffIndex)
+    const diffOfExpected = this.remainingExpected.slice(0, expectedNextCommonIndex)
+    const diffOfActual = this.remainingActual.slice(0, actualNextCommonIndex)
     const diffPart = this.generateDiffString(diffOfExpected, diffOfActual)
-
     this.diffParts.push(diffPart)
-    this.sliceRemaining(expectedRemainingPostDiffIndex, actualRemainingPostDiffIndex)
+
+    this.sliceRemaining(expectedNextCommonIndex, actualNextCommonIndex)
 }
 
-Diff.prototype.findIndexesForNextDiff = function() {
-    let expectedRemainingPostDiffIndex = 0
-    let actualRemainingPostDiffIndex = 0
-    let itemsInExpectedDiff = {}
-    let itemsInActualDiff = {}
+Diff.prototype.findIndexOfNextCommon = function() {
+    let expectedNextCommonIndex = 0
+    let actualNextCommonIndex = 0
+    let expectedFirstOccuranceOfItem = {}
+    let actualFirstOccuranceOfItem = {}
     let maxOfStringLength = Math.max(this.remainingExpected.length, this.remainingActual.length)
-    for (let i = 0; i <= maxOfStringLength; i++) {
-        expectedRemainingPostDiffIndex = i
-        actualRemainingPostDiffIndex = i
-
+    
+    for (let i = 0; i <= maxOfStringLength; i++, expectedNextCommonIndex++, actualNextCommonIndex++) {
         const remainingExpectedCharAti = this.remainingExpected[i]
         const remainingActualCharAti = this.remainingActual[i]
 
-        if (this.remainingExpected.length > i && !itemsInExpectedDiff[remainingExpectedCharAti]) {
-            itemsInExpectedDiff[remainingExpectedCharAti] = i
+        if (this.remainingExpected.length > i && !expectedFirstOccuranceOfItem[remainingExpectedCharAti]) {
+            expectedFirstOccuranceOfItem[remainingExpectedCharAti] = i
         }
-        if (this.remainingActual.length > i && !itemsInActualDiff[remainingActualCharAti]) {
-            itemsInActualDiff[remainingActualCharAti] = i
+        if (this.remainingActual.length > i && !actualFirstOccuranceOfItem[remainingActualCharAti]) {
+            actualFirstOccuranceOfItem[remainingActualCharAti] = i
         }
 
         let b = false
-        if (itemsInActualDiff[remainingExpectedCharAti] !== undefined) {
-            actualRemainingPostDiffIndex = this.remainingActual.indexOf(remainingExpectedCharAti)
+        if (actualFirstOccuranceOfItem[remainingExpectedCharAti] !== undefined) {
+            actualNextCommonIndex = this.remainingActual.indexOf(remainingExpectedCharAti)
             b = true
         }
-        if (itemsInExpectedDiff[remainingActualCharAti] !== undefined) {
-            expectedRemainingPostDiffIndex = this.remainingExpected.indexOf(remainingActualCharAti)
+        if (expectedFirstOccuranceOfItem[remainingActualCharAti] !== undefined) {
+            expectedNextCommonIndex = this.remainingExpected.indexOf(remainingActualCharAti)
             b = true
         }
         if(b) break
     }
-    return {expectedRemainingPostDiffIndex, actualRemainingPostDiffIndex}
+    return {expectedNextCommonIndex, actualNextCommonIndex}
 }
 
 Diff.prototype.sliceRemaining = function(remainingSliceIndex, actualSliceIndex) {
